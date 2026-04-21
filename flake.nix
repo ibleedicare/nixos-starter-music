f0e399f031ac5615cdab99657b2e1c3cafb3a476
ibleedicare <ibleedicare@gmail.com>
2026-04-21 18:05:17 +0200

    feat: Added template default + SilentSDDM

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
 
   outputs = { self, nixpkgs }: {
     templates = {
+      default = {
+        path = ./nixosConfiguration;
+	description = "A very basic nixos configuration with niri and noctalia";
+      };
       nixosConfiguration = {
         path = ./nixosConfiguration;
 	description = "A very basic nixos configuration with niri and noctalia";
