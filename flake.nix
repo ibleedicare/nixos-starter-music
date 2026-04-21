5c11a900457b3ec3c3bbe7238d1812e6544b52dd
ibleedicare <ibleedicare@gmail.com>
2026-04-21 18:18:02 +0200

    Added default

--- flake.nix
+++ flake.nix
@@ -1,19 +1,17 @@
 {
   description = "A very basic flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
-
   outputs = { self, nixpkgs }: {
     templates = {
       default = {
         path = ./nixosConfiguration;
-	description = "A very basic nixos configuration with niri and noctalia";
+        description = "A very basic nixos configuration with niri and noctalia";
       };
       nixosConfiguration = {
         path = ./nixosConfiguration;
-	description = "A very basic nixos configuration with niri and noctalia";
+        description = "A very basic nixos configuration with niri and noctalia";
       };
     };
   };
